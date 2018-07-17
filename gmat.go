aa9c1ce576aa9747ede996fe71a80aac9d6396a5
kurosawa <kurosawa.yk@gmail.com>
2018-07-18 01:21:57 +0900

    gofmt

--- gmat.go
+++ gmat.go
@@ -5,17 +5,17 @@ import (
 	"math/rand"
 )
 
-func Make(n int, m int) ([][]float64) {
+func Make(n int, m int) [][]float64 {
 	z := make([][]float64, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		z[i] = make([]float64, m)
 	}
 	return z
 }
 
-func Make4D(n int, c int, h int, w int) ([][][][]float64) {
+func Make4D(n int, c int, h int, w int) [][][][]float64 {
 	z := make([][][][]float64, n)
-	for i:= range z{
+	for i := range z {
 		z[i] = make([][][]float64, c)
 		for j := range z[i] {
 			z[i][j] = make([][]float64, h)
@@ -27,9 +27,9 @@ func Make4D(n int, c int, h int, w int) ([][][][]float64) {
 	return z
 }
 
-func Make6D(n int, c int, h int, w int, x int, y int) ([][][][][][]float64) {
+func Make6D(n int, c int, h int, w int, x int, y int) [][][][][][]float64 {
 	z := make([][][][][][]float64, n)
-	for i:= range z{
+	for i := range z {
 		z[i] = make([][][][][]float64, c)
 		for j := range z[i] {
 			z[i][j] = make([][][][]float64, h)
@@ -47,24 +47,24 @@ func Make6D(n int, c int, h int, w int, x int, y int) ([][][][][][]float64) {
 	return z
 }
 
-func Trans2D(input [][]float64,n int, c int) ([][]float64) {
+func Trans2D(input [][]float64, n int, c int) [][]float64 {
 	if n >= 2 || c >= 2 {
 		log.Fatal("need to set 2 below param")
 	}
 	inN := len(input)
 	inC := len(input[0])
-	tranmap := map[int]int{0:inN, 1:inC}
-	z := Make(tranmap[n],tranmap[c])
-	for i:= range z {
+	tranmap := map[int]int{0: inN, 1: inC}
+	z := Make(tranmap[n], tranmap[c])
+	for i := range z {
 		for j := range z[i] {
-			var amap = map[int]int{0:i, 1:j}
+			var amap = map[int]int{0: i, 1: j}
 			z[i][j] = input[amap[n]][amap[c]]
 		}
 	}
 	return z
 }
 
-func Trans4D(input [][][][]float64,n int, c int, h int, w int) ([][][][]float64) {
+func Trans4D(input [][][][]float64, n int, c int, h int, w int) [][][][]float64 {
 	if n >= 4 || c >= 4 || h >= 4 || w >= 4 {
 		log.Fatal("need to set 4 below param")
 	}
@@ -72,13 +72,13 @@ func Trans4D(input [][][][]float64,n int, c int, h int, w int) ([][][][]float64)
 	inC := len(input[0])
 	inH := len(input[0][0])
 	inW := len(input[0][0][0])
-	tranmap := map[int]int{0:inN, 1:inC, 2:inH, 3:inW}
-	z := Make4D(tranmap[n],tranmap[c],tranmap[h],tranmap[w])
-	for i:= range z {
+	tranmap := map[int]int{0: inN, 1: inC, 2: inH, 3: inW}
+	z := Make4D(tranmap[n], tranmap[c], tranmap[h], tranmap[w])
+	for i := range z {
 		for j := range z[i] {
 			for k := range z[i][j] {
 				for l := range z[i][j][k] {
-					var amap = map[int]int{0:i, 1:j, 2:k, 3:l}
+					var amap = map[int]int{0: i, 1: j, 2: k, 3: l}
 					z[i][j][k][l] = input[amap[n]][amap[c]][amap[h]][amap[w]]
 				}
 			}
@@ -87,8 +87,7 @@ func Trans4D(input [][][][]float64,n int, c int, h int, w int) ([][][][]float64)
 	return z
 }
 
-
-func Trans6D(input [][][][][][]float64,n int, c int, h int, w int, x int, y int) ([][][][][][]float64) {
+func Trans6D(input [][][][][][]float64, n int, c int, h int, w int, x int, y int) [][][][][][]float64 {
 	if n >= 6 || c >= 6 || h >= 6 || w >= 6 || x >= 6 || y >= 6 {
 		log.Fatal("need to set 6 below param")
 	}
@@ -98,15 +97,15 @@ func Trans6D(input [][][][][][]float64,n int, c int, h int, w int, x int, y int)
 	inW := len(input[0][0][0])
 	inX := len(input[0][0][0][0])
 	inY := len(input[0][0][0][0][0])
-	tranmap := map[int]int{0:inN, 1:inC, 2:inH, 3:inW, 4:inX, 5:inY}
-	z := Make6D(tranmap[n],tranmap[c],tranmap[h],tranmap[w],tranmap[x],tranmap[y])
-	for i:= range z {
+	tranmap := map[int]int{0: inN, 1: inC, 2: inH, 3: inW, 4: inX, 5: inY}
+	z := Make6D(tranmap[n], tranmap[c], tranmap[h], tranmap[w], tranmap[x], tranmap[y])
+	for i := range z {
 		for j := range z[i] {
 			for k := range z[i][j] {
 				for l := range z[i][j][k] {
 					for m := range z[i][j][k][l] {
 						for o := range z[i][j][k][l][m] {
-							var amap = map[int]int{0:i, 1:j, 2:k, 3:l, 4:m, 5:o}
+							var amap = map[int]int{0: i, 1: j, 2: k, 3: l, 4: m, 5: o}
 							z[i][j][k][l][m][o] = input[amap[n]][amap[c]][amap[h]][amap[w]][amap[x]][amap[y]]
 						}
 					}
@@ -117,7 +116,7 @@ func Trans6D(input [][][][][][]float64,n int, c int, h int, w int, x int, y int)
 	return z
 }
 
-func Reshape2D(input [][]float64, reN int, reC int, reH int, reW int) ([][][][]float64) {
+func Reshape2D(input [][]float64, reN int, reC int, reH int, reW int) [][][][]float64 {
 	n := len(input)
 	c := len(input[0])
 	if reW == -1 {
@@ -125,16 +124,16 @@ func Reshape2D(input [][]float64, reN int, reC int, reH int, reW int) ([][][][]f
 	}
 	var input1D []float64
 	tmp := 0
-	for i:= range input {
+	for i := range input {
 		for j := range input[i] {
 			input1D[tmp] = input[i][j]
 			tmp++
 		}
 	}
-	result := Make4D(reN,reC,reH,reW)
+	result := Make4D(reN, reC, reH, reW)
 	tmp = 0
-	for i:= range result {
-		for j:= range result[i] {
+	for i := range result {
+		for j := range result[i] {
 			for k := range result[i][j] {
 				for l := range result[i][j][k] {
 					result[i][j][k][l] = input1D[tmp]
@@ -146,7 +145,7 @@ func Reshape2D(input [][]float64, reN int, reC int, reH int, reW int) ([][][][]f
 	return result
 }
 
-func Reshape2D2D(input [][]float64, reX int, reY int) ([][]float64) {
+func Reshape2D2D(input [][]float64, reX int, reY int) [][]float64 {
 	n := len(input)
 	c := len(input[0])
 	if reX == -1 {
@@ -156,16 +155,16 @@ func Reshape2D2D(input [][]float64, reX int, reY int) ([][]float64) {
 	}
 	var input1D []float64
 	tmp := 0
-	for i:= range input {
+	for i := range input {
 		for j := range input[i] {
 			input1D[tmp] = input[i][j]
 			tmp++
 		}
 	}
-	result := Make(reX,reY)
+	result := Make(reX, reY)
 	tmp = 0
-	for i:= range result {
-		for j:= range result[i] {
+	for i := range result {
+		for j := range result[i] {
 			result[i][j] = input1D[tmp]
 			tmp++
 		}
@@ -173,22 +172,21 @@ func Reshape2D2D(input [][]float64, reX int, reY int) ([][]float64) {
 	return result
 }
 
-
-func Reshape2D6D(input [][]float64, reN int, reC int, reH int, reW int,reX int, reY int) ([][][][][][]float64) {
+func Reshape2D6D(input [][]float64, reN int, reC int, reH int, reW int, reX int, reY int) [][][][][][]float64 {
 	//n := len(input)
 	//c := len(input[0])
 	var input1D []float64
 	tmp := 0
-	for i:= range input {
+	for i := range input {
 		for j := range input[i] {
 			input1D[tmp] = input[i][j]
 			tmp++
 		}
 	}
-	result := Make6D(reN,reC,reH,reW,reX,reY)
+	result := Make6D(reN, reC, reH, reW, reX, reY)
 	tmp = 0
-	for i:= range result {
-		for j:= range result[i] {
+	for i := range result {
+		for j := range result[i] {
 			for k := range result[i][j] {
 				for l := range result[i][j][k] {
 					for m := range result[i][j][k][l] {
@@ -204,7 +202,7 @@ func Reshape2D6D(input [][]float64, reN int, reC int, reH int, reW int,reX int, 
 	return result
 }
 
-func Reshape4D(input [][][][]float64, reX int, reY int) ([][]float64) {
+func Reshape4D(input [][][][]float64, reX int, reY int) [][]float64 {
 	n := len(input)
 	c := len(input[0])
 	h := len(input[0][0])
@@ -216,7 +214,7 @@ func Reshape4D(input [][][][]float64, reX int, reY int) ([][]float64) {
 	}
 	var input1D []float64
 	tmp := 0
-	for i:= range input {
+	for i := range input {
 		for j := range input[i] {
 			for k := range input[i][j] {
 				for l := range input[i][j][k] {
@@ -226,10 +224,10 @@ func Reshape4D(input [][][][]float64, reX int, reY int) ([][]float64) {
 			}
 		}
 	}
-	result := Make(reX,reY)
+	result := Make(reX, reY)
 	tmp = 0
-	for i:= range result {
-		for j:= range result[i] {
+	for i := range result {
+		for j := range result[i] {
 			result[i][j] = input1D[tmp]
 			tmp++
 		}
@@ -237,12 +235,12 @@ func Reshape4D(input [][][][]float64, reX int, reY int) ([][]float64) {
 	return result
 }
 
-func Reshape4D6D(input [][][][]float64, reN int, reC int, reH int, reW int, reX int, reY int) ([][][][][][]float64) {
+func Reshape4D6D(input [][][][]float64, reN int, reC int, reH int, reW int, reX int, reY int) [][][][][][]float64 {
 	//n, c, h, w := Shape4D(input)
 
 	var input1D []float64
 	tmp := 0
-	for i:= range input {
+	for i := range input {
 		for j := range input[i] {
 			for k := range input[i][j] {
 				for l := range input[i][j][k] {
@@ -252,14 +250,14 @@ func Reshape4D6D(input [][][][]float64, reN int, reC int, reH int, reW int, reX 
 			}
 		}
 	}
-	result := Make6D(reN,reC,reH,reW,reX,reY)
+	result := Make6D(reN, reC, reH, reW, reX, reY)
 	tmp = 0
-	for i:= range result {
-		for j:= range result[i] {
-			for k:= range result[i][j] {
-				for l:= range result[i][j][k] {
-					for m:= range result[i][j][k][l] {
-						for n:= range result[i][j][k][l][m] {
+	for i := range result {
+		for j := range result[i] {
+			for k := range result[i][j] {
+				for l := range result[i][j][k] {
+					for m := range result[i][j][k][l] {
+						for n := range result[i][j][k][l][m] {
 							result[i][j][k][l][m][n] = input1D[tmp]
 							tmp++
 						}
@@ -271,8 +269,7 @@ func Reshape4D6D(input [][][][]float64, reN int, reC int, reH int, reW int, reX 
 	return result
 }
 
-
-func Reshape6D(input [][][][][][]float64, reX int, reY int) ([][]float64) {
+func Reshape6D(input [][][][][][]float64, reX int, reY int) [][]float64 {
 	n := len(input)
 	c := len(input[0])
 	h := len(input[0][0])
@@ -284,7 +281,7 @@ func Reshape6D(input [][][][][][]float64, reX int, reY int) ([][]float64) {
 	}
 	var input1D []float64
 	tmp := 0
-	for i:= range input {
+	for i := range input {
 		for j := range input[i] {
 			for k := range input[i][j] {
 				for l := range input[i][j][k] {
@@ -298,10 +295,10 @@ func Reshape6D(input [][][][][][]float64, reX int, reY int) ([][]float64) {
 			}
 		}
 	}
-	result := Make(reX,reY)
+	result := Make(reX, reY)
 	tmp = 0
-	for i:= range result {
-		for j:= range result[i] {
+	for i := range result {
+		for j := range result[i] {
 			result[i][j] = input1D[tmp]
 			tmp++
 		}
@@ -333,7 +330,7 @@ func Shape6D(input [][][][][][]float64) (n int, c int, h int, w int, x int, y in
 	return n, c, h, w, x, y
 }
 
-func Pad4D(input [][][][]float64, pad [][]int) [][][][]float64{
+func Pad4D(input [][][][]float64, pad [][]int) [][][][]float64 {
 	padN := len(pad)
 	padM := len(pad[0])
 	n := len(input)
@@ -343,19 +340,19 @@ func Pad4D(input [][][][]float64, pad [][]int) [][][][]float64{
 	if padN != 4 && padM == 2 {
 		log.Fatal("incorrect padding dim!!")
 	}
-	zN := n+pad[0][0]+pad[0][1]
-	zC := c+pad[1][0]+pad[1][1]
-	zH := h+pad[2][0]+pad[2][1]
-	zW := w+pad[3][0]+pad[3][1]
+	zN := n + pad[0][0] + pad[0][1]
+	zC := c + pad[1][0] + pad[1][1]
+	zH := h + pad[2][0] + pad[2][1]
+	zW := w + pad[3][0] + pad[3][1]
 	z := Make4D(zN, zC, zH, zW)
-	for i:= range z {
+	for i := range z {
 		for j := range z[i] {
 			for k := range z[i][j] {
 				for l := range z[i][j][k] {
-					if (pad[0][0] <= i ) && (pad[1][0] <= j ) &&
-						(pad[2][0] <= k ) && (pad[3][0] <= l) &&
-						(n + pad[0][1] - 1 >= i ) && (c + pad[1][1] - 1 >= j ) &&
-						(h + pad[2][1] - 1 >= k ) && (w + pad[3][1] - 1 >= l) {
+					if (pad[0][0] <= i) && (pad[1][0] <= j) &&
+						(pad[2][0] <= k) && (pad[3][0] <= l) &&
+						(n+pad[0][1]-1 >= i) && (c+pad[1][1]-1 >= j) &&
+						(h+pad[2][1]-1 >= k) && (w+pad[3][1]-1 >= l) {
 						z[i][j][k][l] = input[i-pad[0][0]][j-pad[1][0]][k-pad[2][0]][l-pad[3][0]]
 					}
 				}
@@ -365,10 +362,9 @@ func Pad4D(input [][][][]float64, pad [][]int) [][][][]float64{
 	return z
 }
 
-
-func MakeInit(n int, m int, value float64) ([][]float64) {
+func MakeInit(n int, m int, value float64) [][]float64 {
 	z := make([][]float64, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		z[i] = make([]float64, m)
 	}
 	for i, zArray := range z {
@@ -379,11 +375,11 @@ func MakeInit(n int, m int, value float64) ([][]float64) {
 	return z
 }
 
-func Add(x [][]float64, y[][]float64) ([][]float64) {
+func Add(x [][]float64, y [][]float64) [][]float64 {
 	n := len(x)
 	m := len(x[0])
 	z := make([][]float64, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		z[i] = make([]float64, m)
 
 	}
@@ -395,11 +391,11 @@ func Add(x [][]float64, y[][]float64) ([][]float64) {
 	return z
 }
 
-func AddE(x [][]float64, y float64) ([][]float64) {
+func AddE(x [][]float64, y float64) [][]float64 {
 	n := len(x)
 	m := len(x[0])
 	z := make([][]float64, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		z[i] = make([]float64, m)
 
 	}
@@ -411,11 +407,11 @@ func AddE(x [][]float64, y float64) ([][]float64) {
 	return z
 }
 
-func Sub(x [][]float64, y[][]float64) ([][]float64) {
+func Sub(x [][]float64, y [][]float64) [][]float64 {
 	n := len(x)
 	m := len(x[0])
 	z := make([][]float64, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		z[i] = make([]float64, m)
 
 	}
@@ -427,11 +423,11 @@ func Sub(x [][]float64, y[][]float64) ([][]float64) {
 	return z
 }
 
-func SubE(x [][]float64, y float64) ([][]float64) {
+func SubE(x [][]float64, y float64) [][]float64 {
 	n := len(x)
 	m := len(x[0])
 	z := make([][]float64, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		z[i] = make([]float64, m)
 
 	}
@@ -443,12 +439,11 @@ func SubE(x [][]float64, y float64) ([][]float64) {
 	return z
 }
 
-
-func MulE(x [][]float64, y float64) ([][]float64) {
+func MulE(x [][]float64, y float64) [][]float64 {
 	n := len(x)
 	m := len(x[0])
 	z := make([][]float64, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		z[i] = make([]float64, m)
 	}
 	for i, xArray := range x {
@@ -459,11 +454,11 @@ func MulE(x [][]float64, y float64) ([][]float64) {
 	return z
 }
 
-func Mul(x [][]float64, y [][]float64) ([][]float64) {
+func Mul(x [][]float64, y [][]float64) [][]float64 {
 	n := len(x)
 	m := len(x[0])
 	z := make([][]float64, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		z[i] = make([]float64, m)
 	}
 	for i, xArray := range x {
@@ -474,11 +469,11 @@ func Mul(x [][]float64, y [][]float64) ([][]float64) {
 	return z
 }
 
-func Div(x [][]float64, y [][]float64) ([][]float64) {
+func Div(x [][]float64, y [][]float64) [][]float64 {
 	n := len(x)
 	m := len(x[0])
 	z := make([][]float64, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		z[i] = make([]float64, m)
 	}
 	for i, xArray := range x {
@@ -489,11 +484,11 @@ func Div(x [][]float64, y [][]float64) ([][]float64) {
 	return z
 }
 
-func T(x [][]float64) ([][]float64) {
+func T(x [][]float64) [][]float64 {
 	n := len(x)
 	m := len(x[0])
 	z := make([][]float64, m)
-	for i:=0; i<m; i++{
+	for i := 0; i < m; i++ {
 		z[i] = make([]float64, n)
 	}
 	for i, zArray := range z {
@@ -504,12 +499,11 @@ func T(x [][]float64) ([][]float64) {
 	return z
 }
 
-
-func Apply(x [][]float64, fn func(float64) (float64)) ([][]float64) {
+func Apply(x [][]float64, fn func(float64) float64) [][]float64 {
 	n := len(x)
 	m := len(x[0])
 	z := make([][]float64, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		z[i] = make([]float64, m)
 	}
 	for i, xArray := range x {
@@ -520,7 +514,7 @@ func Apply(x [][]float64, fn func(float64) (float64)) ([][]float64) {
 	return z
 }
 
-func Dot(x [][]float64, y[][]float64) ([][]float64) {
+func Dot(x [][]float64, y [][]float64) [][]float64 {
 	nx := len(x)
 	mx := len(x[0])
 	ny := len(y)
@@ -529,14 +523,14 @@ func Dot(x [][]float64, y[][]float64) ([][]float64) {
 		log.Fatal("mismatch matrix number")
 	}
 	z := make([][]float64, nx)
-	for i:=0; i<nx; i++{
+	for i := 0; i < nx; i++ {
 		z[i] = make([]float64, my)
 
 	}
 	zElem := 0.0
-	for zcol:= 0; zcol < nx ; zcol++ {
-		for zrow:= 0; zrow < my ; zrow++ {
-			for i:= 0; i < mx ; i++ {
+	for zcol := 0; zcol < nx; zcol++ {
+		for zrow := 0; zrow < my; zrow++ {
+			for i := 0; i < mx; i++ {
 				zElem += x[zcol][i] * y[i][zrow]
 			}
 			z[zcol][zrow] = zElem
@@ -546,13 +540,13 @@ func Dot(x [][]float64, y[][]float64) ([][]float64) {
 	return z
 }
 
-func SumRow(x [][]float64) ([][]float64) {
+func SumRow(x [][]float64) [][]float64 {
 	//sum | direction [a,b]
 	//    ^           [a,b]
 	n := len(x)
 	m := len(x[0])
 	sumArray := make([][]float64, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		sumArray[i] = make([]float64, m)
 	}
 	for j := 0; j < m; j++ {
@@ -567,13 +561,13 @@ func SumRow(x [][]float64) ([][]float64) {
 	return sumArray
 }
 
-func SumCol(x [][]float64) ([][]float64) {
+func SumCol(x [][]float64) [][]float64 {
 	//sum -> direction [a,a]
 	//				   [b,b]
 	n := len(x)
 	m := len(x[0])
 	sumArray := make([][]float64, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		sumArray[i] = make([]float64, m)
 	}
 	for j := 0; j < n; j++ {
@@ -588,13 +582,13 @@ func SumCol(x [][]float64) ([][]float64) {
 	return sumArray
 }
 
-func MaxCol(x [][]float64) ([][]float64) {
+func MaxCol(x [][]float64) [][]float64 {
 	//sum -> direction [a,a]
 	//				   [b,b]
 	n := len(x)
 	m := len(x[0])
 	maxArray := make([][]float64, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		maxArray[i] = make([]float64, m)
 	}
 	for j := 0; j < n; j++ {
@@ -611,13 +605,13 @@ func MaxCol(x [][]float64) ([][]float64) {
 	return maxArray
 }
 
-func ArgMaxCol(x [][]float64) ([][]int) {
+func ArgMaxCol(x [][]float64) [][]int {
 	//sum -> direction [a,a]
 	//				   [b,b]
 	n := len(x)
 	m := len(x[0])
 	maxArray := make([][]int, n)
-	for i:=0; i<n; i++{
+	for i := 0; i < n; i++ {
 		maxArray[i] = make([]int, m)
 	}
 	index := 0
@@ -638,7 +632,7 @@ func ArgMaxCol(x [][]float64) ([][]int) {
 
 func RandomNorm2D(r int, c int, init float64) [][]float64 {
 	z := make([][]float64, r)
-	for i:=0; i<r; i++{
+	for i := 0; i < r; i++ {
 		z[i] = make([]float64, c)
 	}
 	for i, zArray := range z {
--- gmat_test.go
+++ gmat_test.go
@@ -1,32 +1,31 @@
 package gmat
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
-
 
 func ExpCheck(zReal [][]float64, zExp [][]float64, t *testing.T) {
 	success := true
-	for i , zArray := range zExp {
+	for i, zArray := range zExp {
 		for j, _ := range zArray {
 			if zExp[i][j] != zReal[i][j] {
-					success = false
+				success = false
 			}
 		}
 	}
 	if !success {
-		fmt.Println("Real:",zReal)
-		fmt.Println("Exp:",zExp)
+		fmt.Println("Real:", zReal)
+		fmt.Println("Exp:", zExp)
 		t.Fatal("failed Test!")
 	}
 }
 
 func ExpCheck4D(zReal [][][][]float64, zExp [][][][]float64, t *testing.T) {
 	success := true
-	for i , zx := range zExp {
+	for i, zx := range zExp {
 		for j, zy := range zx {
-			for k , zz := range zy {
+			for k, zz := range zy {
 				for l, _ := range zz {
 					if zExp[i][j][k][l] != zReal[i][j][k][l] {
 						success = false
@@ -36,62 +35,62 @@ func ExpCheck4D(zReal [][][][]float64, zExp [][][][]float64, t *testing.T) {
 		}
 	}
 	if !success {
-		fmt.Println("Real:",zReal)
-		fmt.Println("Exp:",zExp)
+		fmt.Println("Real:", zReal)
+		fmt.Println("Exp:", zExp)
 		t.Fatal("failed Test!")
 	}
 }
 
-var x =[][]float64{
-	{1,2,3},
-	{4,5,6},
-	{7,8,9},
+var x = [][]float64{
+	{1, 2, 3},
+	{4, 5, 6},
+	{7, 8, 9},
 }
-var y =[][]float64{
-	{8,7,6},
-	{5,4,3},
-	{2,1,0},
+var y = [][]float64{
+	{8, 7, 6},
+	{5, 4, 3},
+	{2, 1, 0},
 }
 
 func TestAddSuccess(t *testing.T) {
 	zExp := [][]float64{
-		{9,9,9},
-		{9,9,9},
-		{9,9,9},
+		{9, 9, 9},
+		{9, 9, 9},
+		{9, 9, 9},
 	}
-	zReal := Add(x,y)
+	zReal := Add(x, y)
 	ExpCheck(zReal, zExp, t)
 }
 
-func minus(x float64) float64{
+func minus(x float64) float64 {
 	return x * -1
 }
 
 func TestApplySuccess(t *testing.T) {
 	zExp := [][]float64{
-		{-1,-2,-3},
-		{-4,-5,-6},
-		{-7,-8,-9},
+		{-1, -2, -3},
+		{-4, -5, -6},
+		{-7, -8, -9},
 	}
-	zReal := Apply(x,minus)
+	zReal := Apply(x, minus)
 	ExpCheck(zReal, zExp, t)
 }
 
 func TestSubSuccess(t *testing.T) {
 	zExp := [][]float64{
-		{-7,-5,-3},
-		{-1,1,3},
-		{5,7,9},
+		{-7, -5, -3},
+		{-1, 1, 3},
+		{5, 7, 9},
 	}
-	zReal := Sub(x,y)
+	zReal := Sub(x, y)
 	ExpCheck(zReal, zExp, t)
 }
 
 func TestTSuccess(t *testing.T) {
 	zExp := [][]float64{
-		{1,4,7},
-		{2,5,8},
-		{3,6,9},
+		{1, 4, 7},
+		{2, 5, 8},
+		{3, 6, 9},
 	}
 	zReal := T(x)
 	ExpCheck(zReal, zExp, t)
@@ -99,59 +98,59 @@ func TestTSuccess(t *testing.T) {
 
 func TestMulESuccess(t *testing.T) {
 	zExp := [][]float64{
-		{3,6,9},
-		{12,15,18},
-		{21,24,27},
+		{3, 6, 9},
+		{12, 15, 18},
+		{21, 24, 27},
 	}
-	zReal := MulE(x,3)
+	zReal := MulE(x, 3)
 	ExpCheck(zReal, zExp, t)
 }
 
 func TestMulSuccess(t *testing.T) {
 	zExp := [][]float64{
-		{8,14,18},
-		{20,20,18},
-		{14,8,0},
+		{8, 14, 18},
+		{20, 20, 18},
+		{14, 8, 0},
 	}
-	zReal := Mul(x,y)
+	zReal := Mul(x, y)
 	ExpCheck(zReal, zExp, t)
 }
 
 func TestDivSuccess(t *testing.T) {
-	var xdiv =[][]float64{
-		{4,6,8},
-		{4,6,8},
-		{4,6,8},
+	var xdiv = [][]float64{
+		{4, 6, 8},
+		{4, 6, 8},
+		{4, 6, 8},
 	}
-	var ydiv =[][]float64{
-		{2,3,4},
-		{2,3,4},
-		{2,3,4},
+	var ydiv = [][]float64{
+		{2, 3, 4},
+		{2, 3, 4},
+		{2, 3, 4},
 	}
 	zExp := [][]float64{
-		{2,2,2},
-		{2,2,2},
-		{2,2,2},
+		{2, 2, 2},
+		{2, 2, 2},
+		{2, 2, 2},
 	}
 	zReal := Div(xdiv, ydiv)
 	ExpCheck(zReal, zExp, t)
 }
 
 func TestDotSuccess(t *testing.T) {
-	xdot :=[][]float64{
-		{1,2,3},
-		{4,5,6},
-		{7,8,9},
+	xdot := [][]float64{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 9},
 	}
-	ydot :=[][]float64{
-		{8,7},
-		{5,4},
-		{2,1},
+	ydot := [][]float64{
+		{8, 7},
+		{5, 4},
+		{2, 1},
 	}
 	zExp := [][]float64{
-		{24,18},
-		{69,54},
-		{114,90},
+		{24, 18},
+		{69, 54},
+		{114, 90},
 	}
 	zReal := Dot(xdot, ydot)
 	ExpCheck(zReal, zExp, t)
@@ -159,9 +158,9 @@ func TestDotSuccess(t *testing.T) {
 
 func TestSumRowSuccess(t *testing.T) {
 	zExp := [][]float64{
-		{12,15,18},
-		{12,15,18},
-		{12,15,18},
+		{12, 15, 18},
+		{12, 15, 18},
+		{12, 15, 18},
 	}
 	zReal := SumRow(x)
 	ExpCheck(zReal, zExp, t)
@@ -169,45 +168,45 @@ func TestSumRowSuccess(t *testing.T) {
 
 func TestSumColSuccess(t *testing.T) {
 	zExp := [][]float64{
-		{6,6,6},
-		{15,15,15},
-		{24,24,24},
+		{6, 6, 6},
+		{15, 15, 15},
+		{24, 24, 24},
 	}
 	zReal := SumCol(x)
 	ExpCheck(zReal, zExp, t)
 }
 
 func TestPad4DSuccess(t *testing.T) {
-	var xpad =[][][][]float64{
+	var xpad = [][][][]float64{
 		{
 			{
-				{4,6,8},
-				{4,6,8},
-				{4,6,8},
+				{4, 6, 8},
+				{4, 6, 8},
+				{4, 6, 8},
 			},
 			{
-				{4,6,8},
-				{4,6,8},
-				{4,6,8},
+				{4, 6, 8},
+				{4, 6, 8},
+				{4, 6, 8},
 			},
 		},
 	}
 
-	var zExp =[][][][]float64{
+	var zExp = [][][][]float64{
 		{
 			{
-				{0,4,6,8,0},
-				{0,4,6,8,0},
-				{0,4,6,8,0},
+				{0, 4, 6, 8, 0},
+				{0, 4, 6, 8, 0},
+				{0, 4, 6, 8, 0},
 			},
 			{
-				{0,4,6,8,0},
-				{0,4,6,8,0},
-				{0,4,6,8,0},
+				{0, 4, 6, 8, 0},
+				{0, 4, 6, 8, 0},
+				{0, 4, 6, 8, 0},
 			},
 		},
 	}
-	var pad = [][]int{{0,0},{0,0},{0,0},{1,1}}
-	zReal := Pad4D(xpad,pad)
+	var pad = [][]int{{0, 0}, {0, 0}, {0, 0}, {1, 1}}
+	zReal := Pad4D(xpad, pad)
 	ExpCheck4D(zReal, zExp, t)
 }
